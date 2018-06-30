9c6595b42e983716907b5a14f2bf0fc5f48f980a
AirGuanZ <AirGuanZ@gmail.com>
2018-06-30 17:27:49 +0800

    代码风格整理

--- 01_char_heart/src/main.rs
+++ 01_char_heart/src/main.rs
@@ -1,16 +1,10 @@
-const IMG_W : i32 = 60;
-const IMG_H : i32 = 31;
-
-const CHARS : [char; 8] = ['.', ':', ':', '-', '+', '=', '*', '#'];
-
 fn heart_equ(x: f64, y: f64, z: f64) -> f64 {
 	let cubic = |s| { s * s * s };
-	let cy = cubic(y);
 	cubic(x * x + 9.0 / 4.0 * z * z + y * y - 1.0)
-		- x * x * cy - 9.0 / 80.0 * z * z * cy
+		- x * x * cubic(y) - 9.0 / 80.0 * z * z * cubic(y)
 }
 
-fn solve_z(func: fn(f64, f64, f64)->f64, x: f64, y: f64) -> f64 {
+fn solve_z(func: fn(f64, f64, f64)->f64, x: f64, y: f64) -> (bool, f64) {
 	let mut lbound = 0.001;
 	let pos_start = func(x, y, 0.0) > 0.0;
 
@@ -18,24 +12,31 @@ fn solve_z(func: fn(f64, f64, f64)->f64, x: f64, y: f64) -> f64 {
 		lbound += 0.001;
 	}
 	
-	if lbound >= 5.0 { std::f64::INFINITY } else { lbound }
+	if lbound >= 5.0 { (false, 0.0) } else { (true, lbound) }
+}
+
+fn shade_pixel(sx: f64, sy: f64) -> char {
+	match solve_z(heart_equ, sx, sy) {
+		(true, sz) => {
+			const CHARS : [char; 8] = ['.', ':', ':', '-', '+', '=', '*', '#'];
+			let c = (-0.557 * sx + 0.743 * sy + 0.335 * sz) / (sx * sx + sy * sy + sz * sz).sqrt() * 1.025;
+			CHARS[(c.min(1.0).max(0.0) * (CHARS.len() - 1) as f64) as usize]
+		},
+		_		   => ' '
+	}
 }
 
 fn main() {
-	for y in 0..IMG_H {
-		for x in 0..IMG_W {
-			let sx = x as f64 / (IMG_W as f64 - 1.0) * 3.0 - 1.5;
-			let sy = - (y as f64 / (IMG_H as f64 - 1.0)) * 3.0 + 1.5;
-			let s = solve_z(heart_equ, sx, sy);
+	const IMG_W: f64 = 3.0;
+	const IMG_H: f64 = 3.0;
+	const SCR_W: i32 = 60;
+	const SCR_H: i32 = 30;
 
-			if s.is_infinite() {
-				print!(" ");
-			} else {
-				let c = (-0.557 * sx + 0.743 * sy + 0.335 * s) /
-						(sx * sx + sy * sy + s * s).sqrt() * 1.025;
-				let c = (c.min(1.0).max(0.0) * (CHARS.len() - 1) as f64) as usize;
-				print!("{}", CHARS[c]);
-			}
+	for y in 0..SCR_H {
+		for x in 0..SCR_W {
+			let sx =   x as f64 / (SCR_W as f64 - 1.0) * IMG_W - (IMG_W / 2.0);
+			let sy = - y as f64 / (SCR_H as f64 - 1.0) * IMG_H + (IMG_H / 2.0);
+			print!("{}", shade_pixel(sx, sy));
 		}
 		println!("");
 	}
